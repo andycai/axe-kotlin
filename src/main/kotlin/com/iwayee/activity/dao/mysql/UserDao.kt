52c9e7ab0f34bfe00bc64548a0071ed0a2365a69
andycai <huayicai@gmail.com>
2021-02-14 08:54:17 +0800

    重构 Dao 层，使用简介的API重写接口

--- src/main/kotlin/com/iwayee/activity/dao/mysql/UserDao.kt
+++ src/main/kotlin/com/iwayee/activity/dao/mysql/UserDao.kt
@@ -8,66 +8,59 @@ object UserDao: MyDao() {
   fun create(user: JsonObject, action: (Long) -> Unit) {
     var fields = "username,password,token,nick,wx_token,wx_nick,sex,phone,email,ip,activities,groups";
     var sql = "INSERT INTO user ($fields) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
-    client?.connection?.onSuccess{conn ->
-      conn.preparedQuery(sql)
-        .execute(Tuple.of(
-          user.getString("username"),
-          user.getString("password"),
-          user.getString("token"),
-          user.getString("nick"),
-          user.getString("wx_token"),
-          user.getString("wx_nick"),
-          user.getInteger("sex"),
-          user.getString("phone"),
-          user.getString("email"),
-          user.getString("ip"),
-          user.getString("activities"),
-          user.getString("groups")
-        ))
-        .onComplete{ ar ->
-          if (ar.succeeded()) {
-            var rows = ar.result()
-            var lastInsertId = rows.property(MySQLClient.LAST_INSERTED_ID)
-            println("Last Insert Id: $lastInsertId")
-            action(lastInsertId)
-          } else {
-            println("Failure: ${ar.cause().message}")
-            action(0L)
-          }
-          conn.close()
-        }
-        .onFailure{ ar ->
+
+    // 批量插入
+    // conn.preparedQuery("INSERT INTO Users (first_name,last_name) VALUES (?, ?)")
+    //      .executeBatch(Arrays.asList(
+    //        Tuple.of("Julien", "Viet"),
+    //        Tuple.of("Emad", "Alblueshi")
+    //      ))
+
+    client?.let {
+      it.preparedQuery(sql).execute(Tuple.of(
+              user.getString("username"),
+              user.getString("password"),
+              user.getString("token"),
+              user.getString("nick"),
+              user.getString("wx_token"),
+              user.getString("wx_nick"),
+              user.getInteger("sex"),
+              user.getString("phone"),
+              user.getString("email"),
+              user.getString("ip"),
+              user.getString("activities"),
+              user.getString("groups")
+      )){ ar ->
+        if (ar.succeeded()) {
+          var rows = ar.result()
+          var lastInsertId = rows.property(MySQLClient.LAST_INSERTED_ID)
+          println("Last Insert Id: $lastInsertId")
+          action(lastInsertId)
+        } else {
+          println("Failure: ${ar.cause().message}")
           action(0L)
-          ar.printStackTrace()
         }
+      }
     }
   }
 
   fun getUserByID(id: Int, action: (JsonObject?) -> Unit) {
     var fields = "id,username,token,nick,wx_token,wx_nick,sex,phone,email,ip,activities,groups,create_at";
     var sql = "SELECT $fields FROM `user` WHERE id = ?"
-    client?.connection?.let {
-      it.onSuccess { conn ->
-        conn.preparedQuery(sql)
-          .execute(Tuple.of(id))
-          .onComplete{ ar ->
-            if (ar.succeeded()) {
-              var rows = ar.result()
-              var jo = JsonObject()
-              for (row in rows) {
-                jo = row.toJson()
-              }
-              action(jo)
-            } else {
-              println("Failure: ${ar.cause().message}")
-              action(null)
-            }
-            conn.close()
+
+    client?.let {
+      it.preparedQuery(sql).execute(Tuple.of(id)) { ar ->
+        if (ar.succeeded()) {
+          var rows = ar.result()
+          var jo = JsonObject()
+          for (row in rows) {
+            jo = row.toJson()
           }
-          .onFailure{ ar ->
-            action(null)
-            ar.printStackTrace()
-          }
+          action(jo)
+        } else {
+          println("Failure: ${ar.cause().message}")
+          action(null)
+        }
       }
     }
   }
@@ -75,28 +68,20 @@ object UserDao: MyDao() {
   fun getUserByName(username: String, action: (JsonObject?) -> Unit) {
     var fields = "id,username,token,nick,wx_token,wx_nick,sex,phone,email,ip,activities,groups,create_at";
     var sql = "SELECT $fields FROM `user` WHERE username = ?"
-    client?.connection?.let {
-      it.onSuccess { conn ->
-        conn.preparedQuery(sql)
-          .execute(Tuple.of(username))
-          .onComplete { ar ->
-            if (ar.succeeded()) {
-              var rows = ar.result()
-              var jo = JsonObject()
-              for (row in rows) {
-                jo = row.toJson()
-              }
-              action(jo)
-            } else {
-              println("Failure: ${ar.cause().message}")
-              action(null)
-            }
-            conn.close()
+
+    client?.let {
+      it.preparedQuery(sql).execute(Tuple.of(username)) { ar ->
+        if (ar.succeeded()) {
+          var rows = ar.result()
+          var jo = JsonObject()
+          for (row in rows) {
+            jo = row.toJson()
           }
-          .onFailure { ar ->
-            action(null)
-            ar.printStackTrace()
-          }
+          action(jo)
+        } else {
+          println("Failure: ${ar.cause().message}")
+          action(null)
+        }
       }
     }
   }
@@ -104,29 +89,23 @@ object UserDao: MyDao() {
   fun getUserByIds(ids: String, action: (JsonObject?) -> Unit) {
     var fields = "id,username,token,nick,wx_token,wx_nick,sex,phone,email,ip,activities,groups";
     var sql = "SELECT $fields FROM `user` WHERE id IN($ids)"
-    client?.connection?.onSuccess{conn ->
-      conn.preparedQuery(sql)
-        .execute()
-        .onComplete{ ar ->
-          if (ar.succeeded()) {
-            var rows = ar.result()
-            if (rows.size() > 0) {
-              var jo = JsonObject()
-              for (row in rows) {
-                jo.put(row.getInteger("id").toString(), row.toJson())
-              }
-              action(jo)
-            } else {
-              println("Failure: ${ar.cause().message}")
-              action(JsonObject())
+
+    client?.let {
+      it.preparedQuery(sql).execute{ ar ->
+        if (ar.succeeded()) {
+          var rows = ar.result()
+          if (rows.size() > 0) {
+            var jo = JsonObject()
+            for (row in rows) {
+              jo.put(row.getInteger("id").toString(), row.toJson())
             }
+            action(jo)
+          } else {
+            println("Failure: ${ar.cause().message}")
+            action(JsonObject())
           }
-          conn.close()
         }
-        .onFailure{ ar ->
-          action(JsonObject())
-          ar.printStackTrace()
-        }
+      }
     }
   }
 
@@ -139,31 +118,25 @@ object UserDao: MyDao() {
       + "groups = ?, "
       + "activities = ?")
     var sql = "UPDATE `user` SET $fields WHERE id = ?"
-    client?.connection?.onSuccess{conn ->
-      conn.preparedQuery(sql)
-        .execute(Tuple.of(
-          user.getString("nick"),
-          user.getString("wx_nick"),
-          user.getString("token"),
-          user.getString("wx_token"),
-          user.getString("ip"),
-          user.getJsonArray("groups").encode(),
-          user.getJsonArray("activities").encode(),
-          id
-        ))
-        .onComplete{ ar ->
-          if (ar.succeeded()) {
-            action(true)
-          } else {
-            println("Failure: ${ar.cause().message}")
-            action(false)
-          }
-          conn.close()
-        }
-        .onFailure{ ar ->
+
+    client?.let {
+      it.preparedQuery(sql).execute(Tuple.of(
+              user.getString("nick"),
+              user.getString("wx_nick"),
+              user.getString("token"),
+              user.getString("wx_token"),
+              user.getString("ip"),
+              user.getJsonArray("groups").encode(),
+              user.getJsonArray("activities").encode(),
+              id
+      )) { ar ->
+        if (ar.succeeded()) {
+          action(true)
+        } else {
+          println("Failure: ${ar.cause().message}")
           action(false)
-          ar.printStackTrace()
         }
+      }
     }
   }
 }
